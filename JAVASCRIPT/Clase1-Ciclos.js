4aebb903d03f6e742a38311de77c4fc8a91559aa
jsegovia <jsegovia@boxcustodia.com>
2023-04-23 21:39:44 -0300

    Agrego ejercicios 1.1 de ciclos de js y 1.1 de excepciones de py de la semana 1

---
+++ JAVASCRIPT/Clase1-Ciclos.js
@@ -0,0 +1,36 @@
+/*
+Clase 1: Ciclos
+Laboratorio III: JAVASCRIPT
+Team: The Informatic Hamsters
+Scrum Master: Nicolas Segovia
+*/
+
+
+
+// 1.1 Ciclo While - Alumno: Nicolas Segovia
+
+let contador  = 0; 
+while(contador < 3){
+    console.log(contador);
+    contador ++;
+}
+
+console.log("Fin de ciclo while");
+
+// 1.2 Ciclo do While - Alumno:
+
+
+
+// 1.3 Ciclo For - Alumno: 
+
+
+
+// 1.4 Palabra reservada Break - Alumno: 
+
+
+
+// 1.5 Palabra reservada Continue - Alumno: 
+
+
+
+// 1.6 Etiquetas Labels - Alumno:
---
+++ PYTHON/Clase1_Excepciones.py
@@ -0,0 +1,30 @@
+'''
+Clase 1: Excepciones
+Laboratorio III: PYTHON
+Team: The Informatic Hamsters
+Scrum Master: Nicolas Segovia
+'''
+
+# 1.1 Manejo de errores o excepciones Parte 1 y parte 2 - Alumno: Nicolas Segovia
+
+try:
+    10/0
+except ZeroDivisionError as ex:
+    print(f'Ocurrio un error: {ex}')
+
+
+# 1.2 Procesamiento de excepciones - Alumno:
+
+
+# 1.3 Procesar clases de exception más específicas - Alumno:
+
+
+# 1.4 Más de procedimientos de excepciones - Alumno:
+
+
+# 1.5 Bloques else y finally al manejar excepciones - Alumno:
+
+
+# 1.6 Creación de clases de Exception personalizadas - Alumno:
+
+
