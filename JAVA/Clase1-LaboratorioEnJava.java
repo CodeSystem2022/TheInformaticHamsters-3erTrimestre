6d1c9215ec16b1ee6651e666e734a89ef2b5f8ac
jsegovia <jsegovia@boxcustodia.com>
2023-04-26 14:41:17 -0300

    agrego clase 1 - ejercicio 1.1 java

---
+++ JAVA/Clase1-LaboratorioEnJava.java
@@ -0,0 +1,32 @@
+/*
+Clase 1: Laboratorio en Java
+Laboratorio III: PYTHON
+Team: The Informatic Hamsters
+Scrum Master: Nicolas Segovia
+*/
+
+// 1.1 Diagrama de Clase UML para este laboratorio: Tienen como tarea hacer este diagrama en VSC,
+// aquí es donde creamos la clase padre DispositivoEntrada - Alumno: Nicolas Segovia 
+
+
+
+// 1.2 Creamos la clase hija Raton y la clase Teclado - Alumno: 
+
+
+// 1.3 Creamos la clase Monitor - Alumno:
+
+
+// 1.4 Creamos la clase Computadora - Alumno:
+
+
+// 1.5 Creamos la clase Orden: Parte 1 y 2 - Alumno:
+
+
+// 1.6 Comenzamos las pruebas creando objetos de cada clase y las agregamos a 
+// la lista de Orden: Parte 1, 2 y 3 - Alumno:
+
+
+// 1.2 Creamos la clase hija Raton y la clase Teclado - Alumno:
+
+
+
