9571150bfab14b70e4f9d6500d462db488fdfd8e
Giuliana Dealbera Etchechoury <giulydealbera@gmail.com>
2023-04-26 17:41:09 -0300

    GiulianaDeEt habilita la Semana 02

---
+++ JAVASCRIPT/Clase2-Arreglos.js
@@ -0,0 +1,20 @@
+/*
+Clase 2: Arreglos
+Laboratorio III: JAVASCRIPT
+Team: The Informatic Hamsters
+Scrum Master: Giuliana Dealbera Etchechoury
+*/
+
+
+
+// 2.1 Arreglos en JavaScript - Alumno: 
+
+// 2.2 Recorremos los elementos de un arreglo - Alumno: Giuliana Dealbera Etchechoury
+
+for(let elemento = 0; elemento < autos2.length; elemento++){
+    console.log(elemento + ": " +autos2[elemento]); 
+}
+
+// 2.3 Modificamos los elementos - Alumno: 
+
+// 2.4 Preguntamos si es un Array - Alumno: 
---
+++ PYTHON/Clase2_ManejoArchivos.py
@@ -0,0 +1,18 @@
+'''
+Clase 2: Manejo de Archivos
+Laboratorio III: PYTHON
+Team: The Informatic Hamsters
+Scrum Master: Giuliana Dealbera Etchechoury
+'''
+
+# 2.1 Introducción a lo que es el manejo de archivos - Alumno: 
+
+# 2.2 Especificar el juego de caracteres de un archivo de texto - Alumno: 
+
+# 2.3 Lectura de archivos - Alumno: 
+
+# 2.4 Más formas de trabajar con archivos - Alumno: 
+
+# 2.5 Uso de WITH, archivos y contexto Manager I - Alumno: 
+
+# 2.6 Uso de WITH, archivos y contexto Manager II - Alumno: 
