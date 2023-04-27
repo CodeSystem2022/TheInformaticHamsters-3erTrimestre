d158b1aa93e32004a5f9d690736c0d6d6c8740eb
Giuliana Dealbera Etchechoury <giulydealbera@gmail.com>
2023-04-27 18:14:47 -0300

    GiulianaDeEt agrega asistencia JAVA semana 1

--- JAVA/Clase1-LaboratorioEnJava.java
+++ JAVA/Clase1-LaboratorioEnJava.java
@@ -16,7 +16,67 @@ Scrum Master: Nicolas Segovia
 // 1.3 Creamos la clase Monitor - Alumno:
 
 
-// 1.4 Creamos la clase Computadora - Alumno:
+// 1.4 Creamos la clase Computadora - Alumno: Giuliana Dealbera Etchechoury
+
+package ar.com.system2023.mundopc;
+
+public class Computadora {
+    private final int idComputadora;
+    private String nombre;
+    private Monitor monitor;
+    private Teclado teclado;
+    private Raton raton;
+    private static int contadorComputadoras;
+
+    private Computadora(){
+        this.idComputadora = ++Computadora.contadorComputadoras;
+    }
+
+    public Computadora(String nombre, Monitor monitor, Teclado teclado, Raton raton){
+        this();
+        this.nombre = nombre;
+        this.monitor = monitor;
+        this.teclado = teclado;
+        this.raton = raton;
+    }
+
+    public int getidComputadora(){
+        return idComputadora;
+    }
+
+    public String getNombre(){
+        return nombre;
+    }
+    public void setNombre(String nombre){
+        this.nombre = nombre;
+    }
+
+    public Monitor getMonitor(){
+        return monitor;
+    }
+    public void setMonitor(Monitor monitor){
+        this.monitor = monitor;
+    }
+
+    public Teclado getTeclado(){
+        return teclado;
+    }
+    public void setTeclado(Teclado teclado){
+        this.teclado = teclado;
+    }
+
+    public Raton getRaton(){
+        return raton;
+    }
+    public void setRaton(Raton raton){
+        this.raton = raton;
+    }
+
+    @Override
+    public String toString(){
+        return "Computadora{" + "idComputadora= " + idComputadora + ", nombre= " + nombre + ", monitor= " + monitor + ", teclado= " + teclado + ", raton= " + raton;
+    }
+}
 
 
 // 1.5 Creamos la clase Orden: Parte 1 y 2 - Alumno:
