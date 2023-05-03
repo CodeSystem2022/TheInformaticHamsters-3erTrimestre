ab2beefabf6163f8d9d21446bece0bd58bcf93f5
Nadia Acosta <nadia.8acosta@gmail.com>
2023-05-02 23:44:32 -0300

    NadiaA-Semana02

--- JAVASCRIPT/Clase2-Arreglos.js
+++ JAVASCRIPT/Clase2-Arreglos.js
@@ -15,6 +15,20 @@ for(let elemento = 0; elemento < autos2.length; elemento++){
     console.log(elemento + ": " +autos2[elemento]); 
 }
 
-// 2.3 Modificamos los elementos - Alumno: 
+// 2.3 Modificamos los elementos del arreglo- Alumno: Nadia Acosta
+autos[1] = 'Volvo';
+console.log(autos[1]);
+
+//Agregamos nuevos valores al arreglo
+autos.push('Audi'); //Agregamos el elemento al final del arreglo
+console.log(autos);
+
+//Otras formas de agregar elementos al arreglo
+autos[autos.length] = 'Porche';
+console.log(autos);
+
+//Tercera forma de agregar elementos teniendo cuidado
+autos[6] = 'Renault';
+console.log(autos); ['Ferrari', 'Volvo', 'BMW', 'Audi', 'Porche', 'Renault']
 
 // 2.4 Preguntamos si es un Array - Alumno: 
--- PYTHON/Clase2_ManejoArchivos.py
+++ PYTHON/Clase2_ManejoArchivos.py
@@ -7,7 +7,19 @@ Scrum Master: Giuliana Dealbera Etchechoury
 
 # 2.1 Introducción a lo que es el manejo de archivos - Alumno: 
 
-# 2.2 Especificar el juego de caracteres de un archivo de texto - Alumno: 
+# 2.2 Especificar el juego de caracteres de un archivo de texto - Alumno: Nadia Acosta
+try:
+    archivo = open('prueba.txt', 'w', encoding='utf8') #La w es de write
+    archivo.write('Programamos con diferentes tipos de archivos, ahora en txt.\n')
+    archivo.write('Los acentos son importantes para las palabras\n')
+    archivo.write('como por ejemplo: acción, ejecución y producción\n')
+    archivo.write('Con eso terminamos.')
+except Exception as e:
+    print(e)
+finally : #siempre se ejecuta
+    archivo.close() #con esto se debe cerrar el archivo
+
+# archivo.write('Todo quedó perfecto'): este es un error!
 
 # 2.3 Lectura de archivos - Alumno: 
 
