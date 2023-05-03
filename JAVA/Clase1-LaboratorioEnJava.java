17ad712afe7751e05cbee880943459005584b4d2
Miguel Rodriguez Saquilan <miguelr.s77@gmail.com>
2023-05-02 23:42:19 -0300

    1_semana_java_1.5_MiguelRS

--- JAVA/Clase1-LaboratorioEnJava.java
+++ JAVA/Clase1-LaboratorioEnJava.java
@@ -19,7 +19,41 @@ Scrum Master: Nicolas Segovia
 // 1.4 Creamos la clase Computadora - Alumno:
 
 
-// 1.5 Creamos la clase Orden: Parte 1 y 2 - Alumno:
+// 1.5 Creamos la clase Orden: Parte 1 y 2 - Alumno:Miguel Rodriguez Saquilan
+package ar.com.system2023.mundopc;
+
+public class Orden {
+    private final int idOrden;
+    private Computadora computadora[]; // Arreglo de objetos
+    private static int contadorOrdenes;
+    private static final  int MAX_COMPUTADORAS = 10;
+    private int contadorComputadora;
+    
+    //Constructor vacio
+    
+    public Orden(){
+        this.idOrden = ++Orden.contadorOrdenes;
+        this.computadora = new Computadora[Orden.MAX_COMPUTADORAS];
+    }
+    
+   //Metodo para agregar una nueva computadora al arreglo
+    public void agregarComputadora(Computadora computadora){
+        if (this.contadorComputadora < Orden.MAX_COMPUTADORAS){
+            this.computadora[this.contadorComputadora++] = computadora;
+        }
+        else{
+            System.out.println("Has superado el limite " +Orden.MAX_COMPUTADORAS);
+        }
+    }
+    //Mostrar orden
+    public void mostarOrden(){
+        System.out.println("Orden  #:"+this.idOrden);
+        System.out.println("Computadoras de la orden #: " + this.idOrden);
+        for(int i = 0; i < this.contadorComputadora; i++){
+            System.out.println(this.computadora[i]);
+        }
+    }
+}
 
 
 // 1.6 Comenzamos las pruebas creando objetos de cada clase y las agregamos a 
