11490877653a81740f024d4463d4dc31c18b8283
Marcelo Alejandro Quispe <quispemarcelo@hotmail.com>
2023-05-03 00:47:19 -0300

    2_semana_JS_2.1_ARREGLOS_MarceloQ

--- JAVASCRIPT/Clase2-Arreglos.js
+++ JAVASCRIPT/Clase2-Arreglos.js
@@ -7,7 +7,40 @@ Scrum Master: Giuliana Dealbera Etchechoury
 
 
 
-// 2.1 Arreglos en JavaScript - Alumno: 
+// 2.1 Arreglos en JavaScript - Alumno: Marcelo Quispe
+// Creacion de Array o arreglos
+// let autos = new Array('Ferrari', 'Renault', 'BMW'); ESTA ES LA VIEJA SINTAXIS
+const autos = ['Ferrari', 'Renault', 'BMW'];
+console.log(autos);
+
+// Recorremos  los elementos de un arreglo
+console.log(autos[0]);
+console.log(autos[2]);
+for(let i = 0; i < autos.length; i++){
+    console.log(i+ ':' +autos[i]);
+}
+
+// Modificamos los elementos del arreglo
+autos[1] = 'Volvo';
+console.log(autos[1]);
+
+// Agregamos nuevos valores al arreglo
+autos.push('Audi'); // Agregamos el elemento al final
+console.log(autos);
+
+// Otras formas de agregar elementos al arreglo
+autos[autos.length] = 'Porche';
+console.log(autos);
+
+// Tercera forma de agregar elementos TENIENDO CUIDADO
+autos[6] = 'Renault';
+console.log(autos);
+
+// Como preguntar si es un Array o Arreglo
+console.log(Array.isArray(autos)); // Devuelve un booleano
+
+console.log(autos instanceof Array); // Preguntamos si la variable es una instancia de la clase Array
+
 
 // 2.2 Recorremos los elementos de un arreglo - Alumno: Giuliana Dealbera Etchechoury
 
