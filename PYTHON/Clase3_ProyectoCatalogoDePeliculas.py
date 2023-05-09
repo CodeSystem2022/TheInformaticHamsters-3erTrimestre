96a1d1dc1d84d6fdd0961441845fd63feb1b9bf5
Juan Pablo Nolan <juan_pablo_nolan@hotmail.com>
2023-05-08 22:47:18 -0300

    Asistencia Semana3 JPNOlan

--- PYTHON/Clase3_ProyectoCatalogoDePeliculas.py
+++ PYTHON/Clase3_ProyectoCatalogoDePeliculas.py
@@ -71,7 +71,34 @@ class CatalogoPeliculas:
 
 # 3.4 Creación test_catalogo_peliculas - Alumno:
 
-# 3.5  Comenzamos con las pruebas ingresando las otras clases - Alumno:
+# 3.5  Comenzamos con las pruebas ingresando las otras clases - Alumno: Juan Pablo Nolan
+
+from dominio.Pelicula import Pelicula
+from  servicio.catalogo_peliculas import CatalogoPeliculas as cp
+
+opcion = None
+while opcion != 4:
+    try:
+        print("Opciones: ")
+        print("1. Agregar pelicula")
+        print("2. Listar películas")
+        print("3. Eliminar catálogo de peliculas")
+        print("4. Salir")
+        opcion = int(input("Digite una opcion de menu (1-4): "))
+        if opcion == 1:
+            nombre_pelicula = input("Digite el nombre de la pelicula: ")
+            pelicula = Pelicula(nombre_pelicula)
+            cp.agregar_peliculas(pelicula)
+        elif opcion == 2:
+            cp.listar_peliculas()
+        elif opcion == 3:
+            cp.eliminar_peliculas()
+    except Exception as e:
+        print(f"Ha ocurrido un error de tipo:{e}")
+        opcion = None
+    else:
+        print("Salimos del programa")
+
 
 
 
