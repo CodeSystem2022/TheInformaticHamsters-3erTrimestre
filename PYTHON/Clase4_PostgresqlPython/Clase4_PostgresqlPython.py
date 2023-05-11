91a03fe06e3cf204a5f7e6d8cca02c29327d6ea9
Giuliana Dealbera Etchechoury <giulydealbera@gmail.com>
2023-05-10 21:30:30 -0300

    GiulianaDeEt agrega asistencia semana 4 py

--- PYTHON/Clase4_PostgresqlPython/Clase4_PostgresqlPython.py
+++ PYTHON/Clase4_PostgresqlPython/Clase4_PostgresqlPython.py
@@ -15,7 +15,8 @@ Esta tabla la llamamos persona y le agregamos 4 columnas: id_persona, nombre, ap
 Al id_persona lo marcamos como una PK.
 Ya teniendo la rama armada, seleccionamos la opción de VIEW y observamos el Query generado, y abajo, la tabla misma
 dónde agregamos unos datos.
-'''
+''' 
+
 
 # 4.3 Consultas con código Query en Postgresql Parte 1 - Alumno: 
 
@@ -24,9 +25,23 @@ dónde agregamos unos datos.
 
 
 
-# 4.6 Conexión hacia la base de datos en Python con el método fetchall() - Alumno: 
-       
+# 4.6 Conexión hacia la base de datos en Python con el método fetchall() - Alumno: Juan Pablo Nolan
 
+import psycopg2 #Esto es para poder conectarnos a la base creada mediante postgress
+
+conexion = psycopg2.connect(
+    user='postgres',
+    password='Lautaro-1',
+    host='127.0.0.1',
+    port='5432',
+    database='test_bd'
+)
+
+cursor = conexion.cursor()
+sentencia = 'SELECT * FROM persona WHERE id_persona = 1'
+cursor.execute(sentencia) #De esta manera ejecutamos la sentencia
+registros = cursor.fetchall() #Recuperamos todos los registros que seran una lista
+print(registros)
 
 # 4.7 Cerramos la conexión y la consulta - Alumno: 
 
