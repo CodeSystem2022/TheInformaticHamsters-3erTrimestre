750500cb3c499ce412578b8fab82bc8de5c54ffb
jsegovia <jsegovia@boxcustodia.com>
2023-05-11 00:12:46 -0300

    Asistencias semana 4 PY, JS y JAVA - Nicolas Segovia

---
+++ PYTHON/Clase4_PostgresqlPython/4.3 Consultas con código Query en Postgresql Parte 1.sql
@@ -0,0 +1,12 @@
+/*
+SELECT * 
+FROM persona 
+WHERE id_persona IN (1, 2, 3);
+*/
+INSERT INTO persona(nombre, apellido, email) 
+VALUES ('Susana', 'Lara', 'slara@gmail.com')
+
+/*
+SELECT *
+FROM persona
+*/
