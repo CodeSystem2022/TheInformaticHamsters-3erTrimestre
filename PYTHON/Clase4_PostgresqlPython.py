093e96a88ecd6b3b8444a66a58012233ca451135
Giuliana Dealbera Etchechoury <giulydealbera@gmail.com>
2023-05-10 21:16:33 -0300

    GiulianaDeEt agrega asistencia semana 4 py

---
+++ PYTHON/Clase4_PostgresqlPython.py
@@ -0,0 +1,33 @@
+'''
+Clase 4: Postgresql y Python
+Laboratorio III: PYTHON
+Team: The Informatic Hamsters
+Scrum Master: Giuliana Paola Diaz Luna
+'''
+
+
+# 4.2 Creamos un base de datos llamada prueba_bd y cargamos una tabla con sus columnas - Alumno: Giuliana Dealbera Etchechoury
+
+'''
+Instalamos el programa Postgresql. Dentro de Databases creamos una base de dato llamada "test_bd".
+Luego nos dirigimos Schemas → Public y agregamos una tabla.
+Esta tabla la llamamos persona y le agregamos 4 columnas: id_persona, nombre, apellido y email.
+Al id_persona lo marcamos como una PK.
+Ya teniendo la rama armada, seleccionamos la opción de VIEW y observamos el Query generado, y abajo, la tabla misma
+dónde agregamos unos datos.
+'''
+
+# 4.3 Consultas con código Query en Postgresql Parte 1 - Alumno: 
+
+
+# 4.4 Consultas con código Query en Postgresql Parte 2 - Alumno: 
+
+
+
+# 4.6 Conexión hacia la base de datos en Python con el método fetchall() - Alumno: 
+       
+
+
+# 4.7 Cerramos la conexión y la consulta - Alumno: 
+
+
