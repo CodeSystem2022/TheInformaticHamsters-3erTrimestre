fa9a3e83ae4f802d0e65efe21fe6fb317c90af31
Giuliana <giulygael2018@gmail.com>
2023-05-13 23:50:06 -0300

    Ingreso ejercicio Python semana 3 - Giuliana Diaz

--- PYTHON/Clase3_ProyectoCatalogoDePeliculas.py
+++ PYTHON/Clase3_ProyectoCatalogoDePeliculas.py
@@ -114,6 +114,23 @@ while opcion != 4:
     else:
         print("Salimos del programa")
 
+# 3.2 Creación de la clase película - Alumno: Giuliana Paola Diaz Luna 
+
+class Pelicula:
+    def __init__(self, nombre):
+        self._nombre = nombre
+
+    def __str__ (self):
+        return f'Pelicula: {self._nombre}'
+
+    @property
+    def nombre(self):
+        return self._nombre
+
+    @nombre.setter 
+    def nombre(self, nombre):
+        self._nombre = nombre
+
 
 
 
