1fe07c0671a0d783920218d3acbcb98ff95f2631
Giuliana Dealbera Etchechoury <giulydealbera@gmail.com>
2023-05-19 00:34:06 -0300

    GiulianaDeEt agrega asistencia semana 6 java

---
+++ JAVA/Clase5 - Conversión de objetos/ClaseAbstract/test/testAbstractas.java
@@ -0,0 +1,9 @@
+package test;
+import domian.*;
+
+public class TestAbstractas{
+    public static void main(String[] args){
+        FiguraGeometrica figura = new Rectangulo('Rectangulo');
+        figura.dibujar();
+    }
+}
