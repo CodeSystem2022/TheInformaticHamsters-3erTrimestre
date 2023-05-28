6e542ef26ecaf8a4e3abb36fe484c07c701642a0
Marcelo Alejandro Quispe <quispemarcelo@hotmail.com>
2023-05-28 20:43:25 -0300

    7_semana_Java_MarceloQ

---
+++ JAVA/Clase7 - Interfaces en Java/Testinterfaces.java
@@ -0,0 +1,20 @@
+// Alumno : Marcelo A. Quispe
+package test;
+
+import accesodatos.*;
+
+
+public class Testinterfaces {
+    public static void main(String[] args) {
+        IAccesoDatos datos = new ImplementacionMySql();
+        //datos.listar();
+        //imprimir (datos);
+        datos =  new ImplementacionOracle();
+        //datos.listar();
+        imprimir(datos);
+    }
+    
+    public static void imprimir(IAccesoDatos datos){
+        datos.listar();
+    }
+}
