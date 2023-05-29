8331e7f5c33201a3edc4d3576d06e1a143375fc4
Giuliana <giulygael2018@gmail.com>
2023-05-29 10:32:38 -0300

    Ingreso ejercicio JS Semana 7 - Giuliana Diaz

---
+++ JAVASCRIPT/Clase6-Clases Parte 1.js
@@ -0,0 +1,49 @@
+/*
+Clase 7:Clases Parte 1
+Laboratorio III: JAVASCRIPT
+Team: The Informatic Hamsters
+Scrum Master: 
+*/
+
+// 6.1 Sintaxis de Clases en JavaScript: Parte 1 - Alumno:
+
+// 6.1 Sintaxis de Clases en JavaScript: Parte 2 - Alumno: Giuliana Paola Diaz Luna
+
+class Persona{
+    // si no se define un constructor jv agrega un constructor vacio de manera automatica
+    constructor(nombre,apellido){
+        this.nombre = nombre; // inicializamos los atributos
+        this.apellido = apellido;         
+    }
+}
+
+// Creacion de objetos
+let persona1 = new Persona ('Giuliana', 'Diaz');
+console.log(persona1);
+
+let persona2 = new Persona ('Fanny', 'Luna');
+console.log(persona2);
+
+
+
+// 6.2 Método Get y Set: Parte Get y Parte Set - Alumno:
+
+
+
+
+
+// 6.3 hoisting y clases: Parte 1 y 2 - Alumno:
+
+
+
+// 6.4 Herencia: Parte 1 y 2 - Alumno: 
+
+
+     
+
+
+
+// 
+
+
+
