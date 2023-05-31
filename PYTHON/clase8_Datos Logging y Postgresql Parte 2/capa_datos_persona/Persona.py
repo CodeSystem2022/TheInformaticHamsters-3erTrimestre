e38fd1e69253ea29d0ca7983aa0b9cbd615d62ca
Giuliana <giulygael2018@gmail.com>
2023-05-31 10:22:58 -0300

    Ingreso ejercicio Python Semana 8 - Giuliana Diaz

--- PYTHON/clase8_Datos Logging y Postgresql Parte 2/capa_datos_persona/Persona.py
+++ PYTHON/clase8_Datos Logging y Postgresql Parte 2/capa_datos_persona/Persona.py
@@ -7,14 +7,24 @@ Scrum Master: Miguel Rodriguez Saquilan
 
 # SI IMPORTAN ALGO SIEMPRE ACA ARRIBA
 
-
+from logger_base import log
 
 
 #  8.1 Creación de la Clase Persona -- Alumno : 
 
+class Persona:
+    def __init__(self,id_persona=None,nombre=None,apellido=None,email=None): # lo dejamos en none para que no sea obligatorio la asignacion de valor a los parámetros
 
 
-# 8.2 Prueba de la Clase Persona - Alumno: 
+# 8.2 Prueba de la Clase Persona - Alumno: Giuliana Paola Diaz Luna
+
+if __name__== '__main__':
+    persona1 = Persona(1,'Juan','Perez','jperez@mail.com')
+    log.debug(persona1)
+    persona2 = Persona(nombre='Jose',apellido='Lopez',email='jlopez@mail.com')
+    log.debug(persona1)
+    persona3= Persona(id_persona = 1)
+    log.debug(persona1)
 
 
 
