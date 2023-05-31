9dc8cfaf7049e315480f59ce414e2f248097ae5c
Nadia Acosta <nadia.8acosta@gmail.com>
2023-05-31 12:26:10 -0300

    NadiaAsistenciaPy

--- PYTHON/clase8_Datos Logging y Postgresql Parte 2/capa_datos_persona/conexiones.py
+++ PYTHON/clase8_Datos Logging y Postgresql Parte 2/capa_datos_persona/conexiones.py
@@ -4,14 +4,31 @@ Laboratorio III: PYTHON
 Team: The Informatic Hamsters
 Scrum Master: Miguel Rodriguez Saquilan
 '''
-
+ 
+import sys
 
 
 # 8.3 Creación de la Clase Conexion: Parte 1 -- Alumno
 
 
 
-#8.3 Creación de la Clase Conexion: Parte 2 -- Alumno
+#8.3 Creación de la Clase Conexion: Parte 2 -- Alumno : Nadia Acosta
 
+@classmethod 
+def obtenerConexion(cls):
+    if cls._conexion is None:
+        try: 
+            cls._conexion = bd.connect(host=cls._HOST,
+                                        user=cls._USERNAME,
+                                        password=cls._PASSWORD,
+                                        port=cls._DB_PORT,
+                                        database=cls._DATABASE)
+            log.debug(f'Conexion exitosa: {cls._conexion}')
+            return cls._conexion
+        except Exception as e:
+            log.error(f'Ocurrio un error: {e}')
+            sys.exit()
+        else:
+            return cls._conexion
 
 # 8.3 Creación de la Clase Conexion: Parte  3 -- Alumno
