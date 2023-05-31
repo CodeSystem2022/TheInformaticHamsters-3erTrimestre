af3d77d4ff1f155494d8bfa7089dfeb5a35e3fd7
jpnolan1979 <juan_pablo_nolan@hotmail.com>
2023-05-31 00:53:23 -0300

    Implementacion MySQL

---
+++ JAVA/Clase7 - Interfaces en Java/ImplementacionMySql.java
@@ -0,0 +1,25 @@
+package accesodatos;
+
+public class ImplementacionMySql implements IAccesoDatos {
+
+    @Override
+    public void insertar() {
+        System.out.println("Insertar desde MySQL");
+    }
+
+    @Override
+    public void listar() {
+        System.out.println("Listar desde MySQL");
+    }
+
+    @Override
+    public void actualizar() {
+        System.out.println("Actualizar desde MySQL");
+    }
+
+    @Override
+    public void eliminar() {
+        System.out.println("Eliminar desde MySQL");
+    }
+    
+}
