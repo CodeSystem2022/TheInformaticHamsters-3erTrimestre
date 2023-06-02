27056363323fc5841ed52be29d182c91a5284005
Miguel Rodriguez Saquilan <miguelr.s77@gmail.com>
2023-06-02 09:32:48 -0300

    mi semana de SM en js

---
+++ JAVASCRIPT/Clase8-Static.js
@@ -0,0 +1,42 @@
+/*
+Clase 8: Static
+Laboratorio III: JAVASCRIPT
+Team: The Informatic Hamsters
+Scrum Master: Miguel Rodriguez Saquilan
+*/
+
+class Persona{
+
+// poner lo que va dentro de esta clase aca.
+
+
+
+
+
+}
+
+
+
+// 8.1 Palabra static con métodos: Parte 1 y 2  - Alumno:
+
+
+
+
+
+// 8.2 Atributos estáticos:  Alumno:
+  
+
+   
+
+
+
+// 8.3 Atributos estáticos vs No estáticos:   Alumno:
+
+
+
+// 8.4 Uso de la palabra static: Parte 1 y 2  - Alumno:
+
+
+
+
+// 8.5 Creación de constantes estáticas - Alumno
