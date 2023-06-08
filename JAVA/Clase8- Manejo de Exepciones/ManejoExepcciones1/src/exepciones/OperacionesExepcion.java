3644a333cc264c3af42fe9286cab504565457d98
Miguel Rodriguez <miguelr.s77@gmail.com>
2023-06-07 22:28:02 -0300

    comiteo java la carpeta de mi semana de SM

---
+++ JAVA/Clase8- Manejo de Exepciones/ManejoExepcciones1/src/exepciones/OperacionesExepcion.java
@@ -0,0 +1,13 @@
+
+
+// ASISTENCIA ALUMNO: JUAN PABLO NOLAN
+package exepciones;
+
+
+public class OperacionesExepcion extends RuntimeException{
+    // constructor
+    public OperacionesExepcion(String mensaje){
+        super(mensaje);
+        
+    }
+}
